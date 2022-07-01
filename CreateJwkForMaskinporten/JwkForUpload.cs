b606dd361469a7d19a8abeb6fbb1a5b89cbd5dc6
Bjørn Isaksen <baisaksen@gmail.com>
2022-07-01 10:14:33 +0200

    Initial add

---
+++ CreateJwkForMaskinporten/JwkForUpload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJwkForMaskinporten
+{
+    public class JwksForUpload
+    {
+        public List<JwkForUpload> keys { get; set; } = new List<JwkForUpload>();
+    }
+    public class JwkForUpload
+    {
+        public string kty { get; set; } = string.Empty;
+        public string e { get; set; } = string.Empty;
+        public string use { get; set; } = string.Empty;
+        public string kid { get; set; } = string.Empty;
+        public string alg { get; set; } = string.Empty;
+        public string n { get; set; } = string.Empty;
+    }
+}
---
+++ CreateJwkForMaskinporten/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CreateJwkForMaskinporten;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+//Will create a new file named <kid>.json, containing the JsonWebKey. The <kid> should be supplied as parameter
+//The key will have a size of 2048
+//The console will output the necessary structure to copy and paste the key for upload to maskinporten integration
+
+string kid = string.Empty;
+
+if (args.Count() == 0)
+{
+    Console.WriteLine("The key id (kid) must be specified as parameter");
+    return;
+}
+
+kid = args[0];
+
+//Generate a public/private key pair.  
+var provider = new RSACryptoServiceProvider(2048);
+var key = new RsaSecurityKey(provider.ExportParameters(true));
+var jwk = JsonWebKeyConverter.ConvertFromRSASecurityKey(key);
+jwk.Kid = kid;
+jwk.Use = "sig";
+jwk.Alg = "RS256";
+
+var options = new JsonSerializerOptions();
+options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+options.PropertyNamingPolicy = new LowerCaseNamingPolicy();
+options.WriteIndented = true;
+
+var json = JsonSerializer.Serialize(jwk, options);
+
+//var filename = $"{jwk.Kid}_{DateTime.Now:yyyy-MM-dd_s}.json";
+var filename = $"{jwk.Kid}.json";
+File.WriteAllText(filename, json);
+
+Console.WriteLine($"RSA key created : {filename}");
+
+var jwks = new JwksForUpload();
+jwks.keys.Add(new JwkForUpload() { alg = jwk.Alg, e = jwk.E, kid = jwk.Kid, kty = jwk.Kty, n = jwk.N, use = jwk.Use });
+var jsonPublicKey = JsonSerializer.Serialize<JwksForUpload>(jwks, options);
+
+Console.WriteLine(Environment.NewLine + "Public key to upload");
+Console.WriteLine(jsonPublicKey);
